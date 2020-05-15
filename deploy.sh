1b8c73844eacdcd13e3098872a22b16b66dda9a5
Avneendra Kanva <avneendrakanva@gmail.com>
2020-05-14 21:35:53 -0700

    Added more screenshots

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,20 @@
 #!/bin/sh
-echo "Creating backups"
-cp -f udagram-deploy/k8s/aws-secret.yaml udagram-deploy/k8s/aws-secret.yaml.backup
-cp -f udagram-deploy/k8s/env-secret.yaml udagram-deploy/k8s/env-secret.yaml.backup
-cp -f udagram-deploy/k8s/env-configmap.yaml udagram-deploy/k8s/env-configmap.yaml.backup
 
-echo "Deploying secrets"
 kubectl delete secret env-secret
 kubectl create -f udagram-deploy/k8s/env-secret.yaml
 kubectl delete secret aws-secret
-#kubectl create -f udagram-deploy/k8s/aws-secret.yaml
-echo "Deploying env"
+kubectl create -f udagram-deploy/k8s/aws-secret.yaml
 kubectl delete configmap env-config 
 kubectl create -f udagram-deploy/k8s/env-configmap.yaml
-echo "Deploying feed"
 kubectl apply -f udagram-deploy/k8s/backend-feed-deployment.yaml
 kubectl apply -f udagram-deploy/k8s/backend-feed-service.yaml
-echo "Deploying user"
 kubectl apply -f udagram-deploy/k8s/backend-user-deployment.yaml
 kubectl apply -f udagram-deploy/k8s/backend-user-service.yaml
-echo "Deploying reverseproxy"
 kubectl apply -f udagram-deploy/k8s/reverseproxy-deployment.yaml
 kubectl apply -f udagram-deploy/k8s/reverseproxy-service.yaml
-echo "Deploying frontend"
 kubectl apply -f udagram-deploy/k8s/frontend-deployment.yaml
 kubectl apply -f udagram-deploy/k8s/frontend-service.yaml
 
-echo "Cleaning up"
-#mv -f udagram-deploy/k8s/aws-secret.yaml.backup udagram-deploy/k8s/aws-secret.yaml
-mv -f udagram-deploy/k8s/env-secret.yaml.backup udagram-deploy/k8s/env-secret.yaml
-mv -f udagram-deploy/k8s/env-configmap.yaml.backup udagram-deploy/k8s/env-configmap.yaml
+sleep 10
+
+kubectl get pods
